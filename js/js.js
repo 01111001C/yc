f308a390d28598a8d26fa7165a7f9bd4df7c2579
SeamosLuzAntigua <91580486+SeamosLuzAntigua@users.noreply.github.com>
2021-11-28 20:04:48 -0600

    ... background

--- js/js.js
+++ js/js.js
@@ -1,15 +1,12 @@
-/*
-window.addEventListener('resize', function(event) {
+var w;
+var h;
 if(window.innerWidth<window.innerHeight){
-    document.getElementById("D_img").width = window.innerWidth;
-    document.getElementById("D_img").height = window.innerWidth;  
-}else{
-    document.getElementById("D_img").width = window.innerHeight;
-    document.getElementById("D_img").height = window.innerHeight;  
-}
+    w=window.innerWidth;h=window.innerWidth;
+}else{w=window.innerHeight;h=window.innerHeight;}
 
-}, true);
-*/
+document.getElementById("D_img").width = w;
+document.getElementById("D_img").height = h;  
+
 
 window.addEventListener('resize', function(event) {
     var w;
